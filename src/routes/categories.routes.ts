d2d5f4d736fdb66de52c6865928d01111aed9670
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-17 22:36:44 -0400

    Create Category class

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
 
 import { Category } from '../model/Category';
 
@@ -10,12 +9,10 @@ const categories: Category[] = [];
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = {
-    id: uuidV4(),
-    name,
-    description,
-    created_at: new Date(),
-  };
+  const category = new Category();
+  category.name = name;
+  category.description = description;
+  category.created_at = new Date();
 
   categories.push(category);
 
