b291a664d6a3c571c73ce68758cdade228f6fe3d
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-17 23:02:20 -0400

    Implements category repository and remove responsability of manipulating data from route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,16 @@
 import { Router } from 'express';
 
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return response.status(201).json({ category });
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
