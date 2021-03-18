54587baac93564da6457b7e2c3bf97c37204195e
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-17 23:13:47 -0400

    Add validation on post route for duplicated name

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -14,6 +14,12 @@ categoriesRoutes.get('/', (request, response) => {
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
