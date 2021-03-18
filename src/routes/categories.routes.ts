4a422c23958257a0a4f04d08486dfe46edd9655b
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-17 22:39:09 -0400

    use object.assign

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -10,13 +10,16 @@ categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
   const category = new Category();
-  category.name = name;
-  category.description = description;
-  category.created_at = new Date();
+
+  Object.assign(category, {
+    name,
+    description,
+    created_at: new Date(),
+  });
 
   categories.push(category);
 
-  return response.status(201).send();
+  return response.status(201).json({ category });
 });
 
 export { categoriesRoutes };
