e7a628120e529f45f8a80d9a7cccc0bd0671a761
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-18 23:28:10 -0400

    Fix missing return type

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -5,7 +5,7 @@ import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-  handle(request: Request, response: Response) {
+  handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
 
     this.createCategoryUseCase.execute({ name, description });
