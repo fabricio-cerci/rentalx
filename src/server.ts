dcec93348a71b5d1ddb3d6b8eae90db8d2512b9e
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-23 19:53:07 -0400

    Initial config of typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
+import './database';
+
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
