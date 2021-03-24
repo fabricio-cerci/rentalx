a55a94e73a66d2300bdf32073a0842723bf56046
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-23 22:33:26 -0400

    Add comments

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,7 +2,9 @@ import { CategoriesRepository } from '../../repositories/implementations/Categor
 import { CreateCategoryController } from './CreateCategoryController';
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-// Para conseguir controlar o momento que esse arquivo vai ser executado
+// Para conseguir controlar o momento que esse arquivo vai ser executado pois quando está
+// fora de uma função quando o arquivo é importado o código é rodado e ai o getRepository
+// acontece antes do banco em si levantar.
 export default (): CreateCategoryController => {
   const categoriesRepository = new CategoriesRepository();
 
