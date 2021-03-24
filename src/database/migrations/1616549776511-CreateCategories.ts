d78560879eec88f0b147329b993c715bfb99f384
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-23 21:54:16 -0400

    implements migration pattern

---
+++ src/database/migrations/1616549776511-CreateCategories.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn } from 'typeorm';
+
+export default class CreateCategories1616549776511
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
