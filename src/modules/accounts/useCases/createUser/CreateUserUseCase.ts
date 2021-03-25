586cd5b2b5aef491b6fdfee2bd6ff4f934adfe66
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-24 20:27:30 -0400

    implements verification if user email is in use

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
 import { IUsersRepository } from '../../repositories/IUsersRepository';
@@ -22,10 +23,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: IRequest): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
