4d977714ab48e0c05d2b0ac0f9cf8fd7473480c0
Fabrício Cerci <fabriciocerci@gmail.com>
2021-03-31 23:30:52 -0400

    Create example base test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,15 @@
+describe('Criar categoria', () => {
+  it('Espero que 2 + 2 seja 4', () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+
+    expect(soma).toBe(resultado);
+  });
+
+  it('Espero que 2+2 não seja 5', () => {
+    const soma = 2 + 2;
+    const resultado = 5;
+
+    expect(soma).not.toBe(resultado);
+  });
+});
