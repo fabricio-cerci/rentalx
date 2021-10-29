74ab71ac700c47d9095ab99a7daf00926f0c808e
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-10-29 00:33:12 -0400

    🏷️ type: update ICreateCarDTO to implements new properties

--- src/modules/cars/dtos/ICreateCarDTO.ts
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,4 +1,7 @@
+import { Specification } from '../infra/typeorm/entities/Specification';
+
 interface ICreateCarDTO {
+  id?: string;
   name: string;
   description: string;
   daily_rate: number;
@@ -6,4 +9,7 @@ interface ICreateCarDTO {
   fine_amount: number;
   brand: string;
   category_id: string;
+  specifications?: Specification[];
 }
+
+export { ICreateCarDTO };
