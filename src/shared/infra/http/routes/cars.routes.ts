5b0377dfb33d37919d44de16041dd07e185afa29
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-10-28 23:11:35 -0400

    🚚 routes: implements list available cars controller and its routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/useCases/listCars/ListAvailableCarsController';
 import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 import { ensureAutheticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
+
+carsRoutes.get('/available', listAvailableCarsController.handle);
 
 carsRoutes.post(
   '/',
