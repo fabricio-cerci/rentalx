6416616d564b26d10120ccdd4053126ccf717513
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-10-28 23:20:38 -0400

    🗃️ migration: implements migration for many to many table specifications_cars

---
+++ src/shared/infra/typeorm/migrations/1635477209004-CreateSpecificationsCars.ts
@@ -0,0 +1,55 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateSpecificationsCars1635477209004
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'specifications_cars',
+        columns: [
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'specification_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          new TableForeignKey({
+            name: 'FKSpecificationCar',
+            columnNames: ['specification_id'],
+            referencedTableName: 'specifications',
+            referencedColumnNames: ['id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          }),
+          new TableForeignKey({
+            name: 'FKCarSpecification',
+            columnNames: ['car_id'],
+            referencedTableName: 'specifications',
+            referencedColumnNames: ['id'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL',
+          }),
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('specifications_cars');
+  }
+}
