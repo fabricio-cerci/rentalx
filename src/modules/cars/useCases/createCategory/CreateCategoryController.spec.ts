5e22ea5025f007f58cc623ab1c38e47d340e6db7
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-10-30 17:59:54 -0400

    ✅ test: update create category controller integration test, adding connection to the database, running migration, creating admin user and dropping database and connection after all and add token to the test request

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,13 +1,47 @@
+import { hash } from 'bcryptjs';
 import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
 
 import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
 
-describe('Create Category Controller', async () => {
+let connection: Connection;
+
+describe('Create Category Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash('admin123', 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license) 
+      VALUES('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')`,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
   it('should to be able to create a new category', async () => {
-    const response = await request(app).post('/categories').send({
-      name: 'Category Supertest',
-      description: 'Category Supertest',
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@rentx.com.br',
+      password: 'admin123',
     });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category Supertest',
+        description: 'Category Supertest',
+      })
+      .set({ Authorization: `Bearer ${token}` });
 
     expect(response.status).toBe(201);
   });
