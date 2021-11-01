9b027d5d9e7255c69de189f81705b4315a56d9f7
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-01 11:40:35 -0400

    ✨ usecase: implements devolution rental use case

---
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -0,0 +1,69 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+}
+
+@injectable()
+class DevolutionRentalUseCase {
+  constructor(
+    @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
+    @inject('DateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
+
+  public async execute({ id }: IRequest): Promise<Rental> {
+    const rental = await this.rentalsRepository.findById(id);
+    const car = await this.carsRepository.findById(rental.car_id);
+    const minimumDaily = 1;
+
+    if (!rental) {
+      throw new AppError("Rental doesn't exists", 404);
+    }
+
+    // Verificar o tempo de aluguel
+    const dateNow = this.dateProvider.dateNow();
+
+    let daily = this.dateProvider.compareInDays(
+      rental.start_date,
+      this.dateProvider.dateNow(),
+    );
+
+    if (daily <= 0) {
+      daily = minimumDaily;
+    }
+
+    const delay = this.dateProvider.compareInDays(
+      dateNow,
+      rental.expected_return_date,
+    );
+
+    let total = 0;
+
+    if (delay > 0) {
+      const calculateFine = delay * car.fine_amount;
+      total = calculateFine;
+    }
+
+    total += daily * car.daily_rate;
+
+    rental.end_date = this.dateProvider.dateNow();
+    rental.total = total;
+
+    await this.rentalsRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
+
+    return rental;
+  }
+}
+
+export { DevolutionRentalUseCase };
