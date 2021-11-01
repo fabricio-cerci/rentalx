9b511671d06391a36060b092ec1dacd64709ed25
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-01 11:40:12 -0400

    ✨ provider: implements method compareInDays on DateProvider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,5 +1,6 @@
 interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
+  compareInDays(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
   dateNow(): Date;
 }
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -13,6 +13,12 @@ class DayjsDateProvider implements IDateProvider {
     return dayjs(end_date_utc).diff(start_date_utc, 'hours');
   }
 
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUTC(end_date);
+    const start_date_utc = this.convertToUTC(start_date);
+
+    return dayjs(end_date_utc).diff(start_date_utc, 'days');
+  }
   convertToUTC(date: Date): string {
     return dayjs(date).utc().local().format();
   }
