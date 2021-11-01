51b3b2bfe59363f55875a4dd3b679c6f8ee9669e
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-01 19:58:35 -0400

    🚚 routes: implements list rentals by user controller and route

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,12 +2,20 @@ import { Router } from 'express';
 
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/createRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalsRoutes = Router();
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
+
+rentalsRoutes.get(
+  '/user',
+  ensureAuthenticated,
+  listRentalsByUserController.handle,
+);
 
 rentalsRoutes.post('/', ensureAuthenticated, createRentalController.handle);
 
