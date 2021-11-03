e2bc78757a21d54908107e93b5672cc00f5352d0
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-02 20:13:47 -0400

    🔧 config file: implements auth config file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '89a7055c3ba45d5b7510aab432476b0f',
+  expires_in_token: '15m',
+  secret_refresh_token: '405499b49cb5027d2bf03b0ac3dbeeff',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
