e1e80eed34bdb5f3d01dc4e643acfcd1821a729f
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-02 21:30:00 -0400

    🚚 routes: update authenticate routes to add refresh-token route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -12,7 +12,7 @@ const router = Router();
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
-router.use('/sessions', authenticateRoutes);
+router.use(authenticateRoutes);
 router.use('/cars', carsRoutes);
 router.use('/rentals', rentalsRoutes);
 
