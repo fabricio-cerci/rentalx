62bb481f4844c2ebbce767b8292f1c2efac722f1
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 13:29:03 -0400

    ⚡️ (improve): implements static routes to app to serve files

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
+import upload from '@config/upload';
 import { AppError } from '@shared/errors/AppError';
 import createConnection from '@shared/infra/typeorm';
 
@@ -17,6 +18,9 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+
+app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
