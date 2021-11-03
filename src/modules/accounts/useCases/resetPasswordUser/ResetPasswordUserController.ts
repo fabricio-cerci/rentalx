4e5f7ce632eb7bdf3cf4d676107ddb1513252897
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 00:13:28 -0400

    🚚 route: implements reset password user controller and its routes

---
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ResetPasswordUserUseCase } from './ResetPasswordUserUseCase';
+
+class ResetPasswordUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { token } = request.query;
+    const { password } = request.body;
+
+    const resetPasswordUserUseCase = container.resolve(
+      ResetPasswordUserUseCase,
+    );
+
+    await resetPasswordUserUseCase.execute({
+      token: token as string,
+      password,
+    });
+
+    return response.send();
+  }
+}
+
+export { ResetPasswordUserController };
--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRoutes.post('/reset', resetPasswordUserController.handle);
 
 export { passwordRoutes };
