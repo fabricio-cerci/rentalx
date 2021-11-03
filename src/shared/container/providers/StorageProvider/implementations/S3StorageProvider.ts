15069f3ac5e32e8d8ef83c1902368589e903e855
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 12:49:29 -0400

    ✨ provider: implements S3StorageProvider

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,48 @@
+import { S3 } from 'aws-sdk';
+import fs from 'fs';
+import mime from 'mime';
+import { resolve } from 'path';
+
+import upload from '@config/upload';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
+
+class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new S3({
+      region: process.env.AWS_BUCKET_REGION,
+    });
+  }
+
+  async save(file: string, folder: string): Promise<string> {
+    const originalName = resolve(upload.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalName);
+
+    await this.client
+      .putObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType: mime.getType(originalName),
+      })
+      .promise();
+
+    await fs.promises.unlink(originalName);
+
+    return file;
+  }
+
+  async delete(file: string, folder: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export { S3StorageProvider };
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,9 +1,15 @@
 import { container } from 'tsyringe';
 
 import { LocalStorageProvider } from './implementations/LocalStorageProvider';
+import { S3StorageProvider } from './implementations/S3StorageProvider';
 import { IStorageProvider } from './IStorageProvider';
+
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  LocalStorageProvider,
+  diskStorage[process.env.DISK],
 );
