ac7eced795ed03e62c8de77f4f788f138840275d
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-02 21:48:23 -0400

    ⚡️ improve: implements method addHours to dateProvider and implements it on dayjs provider

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -4,6 +4,7 @@ interface IDateProvider {
   convertToUTC(date: Date): string;
   dateNow(): Date;
   addDays(days: number): Date;
+  addHours(hours: number): Date;
 }
 
 export { IDateProvider };
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -30,6 +30,10 @@ class DayjsDateProvider implements IDateProvider {
   addDays(days: number): Date {
     return dayjs().add(days, 'days').toDate();
   }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, 'hours').toDate();
+  }
 }
 
 export { DayjsDateProvider };
