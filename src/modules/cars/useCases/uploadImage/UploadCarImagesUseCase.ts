e106eaa012e14ecc2a4e8bf4ad982f0831babdc4
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 13:28:38 -0400

    ♻️ refactor: UploadCarImagesUseCase to use StorageProvider to upload images

--- src/modules/cars/useCases/uploadImage/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadImage/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 
 interface IRequest {
   car_id: string;
@@ -12,11 +13,14 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImagesRepository')
     private carsImagesRepository: ICarsImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   public async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async image => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, 'cars');
     });
   }
 }
