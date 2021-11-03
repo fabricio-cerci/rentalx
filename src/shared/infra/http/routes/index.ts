555d777528a91f3df8cd0e8adde259ad990fe57c
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-02 22:24:29 -0400

    🚚 route: implements SendForgotPasswordMailController and its routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { passwordRoutes } from './password.routes';
 import { rentalsRoutes } from './rentals.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -13,6 +14,7 @@ router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
 router.use(authenticateRoutes);
+router.use('/password', passwordRoutes);
 router.use('/cars', carsRoutes);
 router.use('/rentals', rentalsRoutes);
 
