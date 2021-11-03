7ef6e49aa8d31c7989f9e163d92790a1040aaad4
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 09:22:34 -0400

    ♻️  (refactor) : refresh token to right way

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload extends JwtPayload {
   email: string;
 }
 
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
   constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider,
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { sub, email } = verify(token, auth.secret_refresh_token) as IPayload;
 
     const user_id = sub;
@@ -51,7 +56,12 @@ class RefreshTokenUseCase {
       expires_date: refresh_token_expires_date,
     });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return { refresh_token, token: newToken };
   }
 }
 
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import auth from '@config/auth';
-import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface IPayload {
@@ -15,7 +15,7 @@ export async function ensureAuthenticated(
   next: NextFunction,
 ): Promise<void> {
   const authHeader = request.headers.authorization;
-  const userTokensRepository = new UsersTokensRepository();
+  const usersRepository = new UsersRepository();
 
   if (!authHeader) {
     throw new AppError('Token missing', 401);
@@ -24,22 +24,16 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token,
-    ) as IPayload;
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
-    const userToken = await userTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token,
-    );
+    const user = await usersRepository.findById(user_id);
 
-    if (!userToken) {
-      throw new AppError('User token does not exists!', 401);
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
     }
 
     request.user = {
-      id: userToken.user_id,
+      id: user.id,
     };
 
     return next();
