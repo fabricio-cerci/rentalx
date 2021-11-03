b1e3e8f7572c5e9ee4b81f90c14b84df80665051
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 12:27:37 -0400

    🐛 fix: just change interface import to absolute import

--- src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -2,8 +2,7 @@ import fs from 'fs';
 import { resolve } from 'path';
 
 import upload from '@config/upload';
-
-import { IStorageProvider } from '../IStorageProvider';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 
 class LocalStorageProvider implements IStorageProvider {
   async save(file: string, folder: string): Promise<string> {
