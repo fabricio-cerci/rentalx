0afe076ae81421ee0cde6b0554ac438f03ebd39d
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-03 13:29:21 -0400

    🚚 route: implements ProfileUserController and it route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUser/ProfileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
@@ -12,6 +13,7 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('/', createUserController.handle);
 
@@ -21,5 +23,7 @@ usersRoutes.patch(
   uploadAvatar.single('avatar'),
   updateUserAvatarController.handle,
 );
+
+usersRoutes.get('/profile', ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
