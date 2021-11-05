ba21296ac231d4341fdf2a7d2ffe217814adeed1
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2021-11-05 00:42:49 -0400

    🐛 fix: babel.config.js file

--- babel.config.js
+++ babel.config.js
@@ -8,10 +8,10 @@ module.exports = {
       'module-resolver',
       {
         alias: {
-          '@modules': ['./src/modules'],
-          '@config': ['./src/config'],
-          '@shared': ['./src/shared'],
-          '@utils': ['./src/utils'],
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@shared': './src/shared',
+          '@utils': './src/utils',
         },
       },
     ],
